283c3aed4fa41f53bef0135a9a6c8f2f0d9bb23e
Amyat103 <zhoudavid7312@gmail.com>
2024-02-17 17:24:27 -0500

    finish js exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr === null) {
+    return 0;
+  }
+  let sum = 0;
+  arr.forEach(item => {
+    sum += item;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr === null) {
+    return 0;
+  }
+  let sum = 1;
+  arr.forEach(item => {
+    sum *= item;
+  })
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let sum = 1;
+  for (let i = 0; i < b; i++) {
+    sum *= a;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1;
+  for (let i = 1; i <= a; i++) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line

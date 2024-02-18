ae65a44dd38e95b91997f92c0bd56c73786f19ba
Amyat103 <zhoudavid7312@gmail.com>
2024-02-17 21:07:26 -0500

    finish exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
 
+    let oldest = arr.sort((a, b) => {
+        let Aage = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        let Bage = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+        
+        return Bage - Aage;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line

ddd841b73cce191628d4b2515864f55651efc2da
Amyat103 <zhoudavid7312@gmail.com>
2024-02-17 19:56:26 -0500

    finish js exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    s = s.toLowerCase();
+    s = s.replace(/[\W_]+/g,"")
+    if (s.length == 1) {
+        return true;
+    } else if (s.length == 0) {
+        return false;
+    }
+    let back = 1;
+    for (let i = 0; i <= s.length / 2; i++) {
+        if (s.charAt(i) != s.charAt(s.length-back)) {
+            return false;
+        }
+        back ++;
+    }
+    return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
